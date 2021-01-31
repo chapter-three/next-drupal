327fb7ea0996eb5ecefa416630d11c9597c5f4be
arshad <arshad@chummun.com>
2021-01-31 22:00:47 +0400

    fix: return null if no body passed

--- packages/next-drupal/src/deserialize.ts
+++ packages/next-drupal/src/deserialize.ts
@@ -3,5 +3,7 @@ import Jsona from "jsona"
 const dataFormatter = new Jsona()
 
 export function deserialize(body, options?) {
+  if (!body) return null
+
   return dataFormatter.deserialize(body, options)
 }

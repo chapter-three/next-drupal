21251f91079ebebbd0e6e9c9f451a1590d8ed82e
arshad <arshad@chummun.com>
2021-04-06 12:26:54 +0400

    feat(www): add search shortcut

--- www/src/components/doc-search.tsx
+++ www/src/components/doc-search.tsx
@@ -1,7 +1,11 @@
 import * as React from "react"
 import { VisuallyHidden } from "reflexjs"
 
+const KEYBOARD_SHORTCUT_KEY = "/"
+
 export function DocSearch({ ...props }) {
+  const input = React.useRef<HTMLInputElement>(null)
+
   React.useEffect(() => {
     if (window.docsearch) {
       try {
@@ -16,8 +20,27 @@ export function DocSearch({ ...props }) {
     }
   }, [])
 
+  React.useEffect(() => {
+    const inputs = ["input", "select", "button", "textarea"]
+
+    function handleKeyDown(event) {
+      if (
+        document.activeElement &&
+        inputs.indexOf(document.activeElement.tagName.toLowerCase()) === -1
+      ) {
+        if (event.key === KEYBOARD_SHORTCUT_KEY) {
+          event.preventDefault()
+          input.current.focus()
+        }
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+    return () => window.removeEventListener("keydown", handleKeyDown)
+  }, [])
+
   return (
-    <div {...props}>
+    <div position="relative" {...props}>
       <VisuallyHidden>
         <label htmlFor="docsearch-input">Search</label>
       </VisuallyHidden>
@@ -33,7 +56,24 @@ export function DocSearch({ ...props }) {
         w="100%|275px"
         bg="muted"
         flex="1"
+        ref={input}
       />
+      <span
+        position="absolute"
+        top="1"
+        right="1"
+        bg="background"
+        borderWidth="1"
+        width="7"
+        height="7"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="lg"
+        fontSize="sm"
+      >
+        {KEYBOARD_SHORTCUT_KEY}
+      </span>
     </div>
   )
 }

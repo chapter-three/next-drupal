424c95dd9e827d63bed3c041f922291c1fafdac7
arshad <arshad@chummun.com>
2021-06-14 20:56:50 +0400

    feat(next): add next_extras for content_translations

---
+++ modules/next/modules/next_extras/src/Plugin/Field/FieldType/ContentTranslationsFieldItemList.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\next_extras\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+
+class ContentTranslationsFieldItemList extends FieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function computeValue() {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $entity = $this->getEntity();
+
+    if (!$entity->isTranslatable()) {
+      return NULL;
+    }
+
+    $langcodes = array_keys($entity->getTranslationLanguages());
+    foreach ($langcodes as $delta => $langcode) {
+      $translation = $entity->getTranslation($langcode);
+      $this->list[$delta] = $this->createItem($delta, [
+        'label' => $translation->label(),
+        'path' => $translation->toUrl()->toString(),
+        'langcode' => $langcode,
+      ]);
+    }
+  }
+
+}
---
+++ modules/next/modules/next_extras/src/Plugin/Field/FieldType/ContentTranslationsItem.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\next_extras\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'content_translations' entity field type.
+ *
+ * @FieldType(
+ *   id = "content_translations",
+ *   label = @Translation("Content translations"),
+ *   description = @Translation("An entity field containing data about content translations."),
+ *   no_ui = TRUE,
+ *   list_class = "\Drupal\next_extras\Plugin\Field\FieldType\ContentTranslationsFieldItemList",
+ *   cardinality = \Drupal\Core\Field\FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED
+ * )
+ */
+class ContentTranslationsItem extends FieldItemBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['label'] = DataDefinition::create('string')
+      ->setLabel(t('Label'));
+
+    $properties['alias'] = DataDefinition::create('string')
+      ->setLabel(t('Path alias'));
+
+    $properties['langcode'] = DataDefinition::create('string')
+      ->setLabel(t('Language Code'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    return $this->label === NULL && $this->alias === '' && $this->langcode === NULL;
+  }
+
+}

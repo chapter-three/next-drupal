04f2198a5a92251e420ceb1285bc6f39ded5efad
shadcn <arshad@chummun.com>
2022-01-31 17:40:17 +0400

    feat: add and configure turbo

--- examples/example-no-preview/next.config.js
+++ examples/example-no-preview/next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+const withTM = require("next-transpile-modules")(["next-drupal"])
+
+module.exports = withTM({
+  reactStrictMode: true,
   images: {
     domains: [process.env.NEXT_IMAGE_DOMAIN],
   },
-}
+})

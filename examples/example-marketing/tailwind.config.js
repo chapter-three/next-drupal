630fc24e126560f1dccc6f495079e895fa4f70ca
shadcn <arshad@chummun.com>
2022-01-31 16:49:16 +0400

    fix(example-marketing): remove tailwind forms

--- examples/example-marketing/tailwind.config.js
+++ examples/example-marketing/tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography")],
 }

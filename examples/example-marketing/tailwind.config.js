943d7f1c9a37418125053e897b67b070065479c4
shadcn <arshad@chummun.com>
2022-01-31 18:15:53 +0400

    fix(example-marketing): update tailwindcss

--- examples/example-marketing/tailwind.config.js
+++ examples/example-marketing/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },

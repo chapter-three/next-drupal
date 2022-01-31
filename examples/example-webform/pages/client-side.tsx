36b58e82425af10fcdc07e0cec829383f90b4c0f
shadcn <arshad@chummun.com>
2022-01-31 16:58:17 +0400

    fix(example-webform): update packages

--- examples/example-webform/pages/client-side.tsx
+++ examples/example-webform/pages/client-side.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 import { getResourceCollection, DrupalTaxonomyTerm } from "next-drupal"
 import { useForm } from "react-hook-form"
 import * as yup from "yup"
-import { yupResolver } from "@hookform/resolvers/yup/dist/yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { contactFormSchema } from "../validations/contact"
 
 type FormData = yup.TypeOf<typeof contactFormSchema>

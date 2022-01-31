e9b583f645e2a8aa9a5bf70cc132fc31b2ebb6b3
shadcn <arshad@chummun.com>
2022-01-31 17:45:47 +0400

    fix(example-marketing): add tailwindcss forms

--- examples/example-marketing/tailwind.config.js
+++ examples/example-marketing/tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 }

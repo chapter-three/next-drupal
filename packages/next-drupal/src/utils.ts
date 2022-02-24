325e368534720c8350afdf381f12b982f9a5d196
shadcn <arshad@chummun.com>
2022-02-24 16:10:49 +0400

    feat(next-drupal): add UNSTABLE_DRUPAL_ACCESS_TOKEN

--- packages/next-drupal/src/utils.ts
+++ packages/next-drupal/src/utils.ts
@@ -85,6 +85,17 @@ export async function buildHeaders({
   accessToken?: AccessToken
   headers?: RequestInit["headers"]
 } = {}): Promise<RequestInit["headers"]> {
+  // This allows an access_token (preferrably long-lived) to be set directly on the env.
+  // This reduces the number of OAuth call to the Drupal server.
+  // Intentionally marked as unstable for now.
+  if (process.env.UNSTABLE_DRUPAL_ACCESS_TOKEN) {
+    headers[
+      "Authorization"
+    ] = `Bearer ${process.env.UNSTABLE_DRUPAL_ACCESS_TOKEN}`
+
+    return headers
+  }
+
   const token = accessToken || (await getAccessToken())
   if (token) {
     headers["Authorization"] = `Bearer ${token.access_token}`

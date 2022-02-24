bf402b1a8ae4937cb91cd636c1256161da7d612e
shadcn <arshad@chummun.com>
2022-02-24 15:50:56 +0400

    feat(example-marketing): add revalidate api

---
+++ examples/example-marketing/pages/api/revalidate.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  let slug = request.query.slug as string
+  const secret = request.query.secret as string
+
+  // Validate secret.
+  if (secret !== process.env.DRUPAL_PREVIEW_SECRET) {
+    return response.status(401).json({ message: "Invalid secret." })
+  }
+
+  // Validate slug.
+  if (!slug) {
+    return response.status(400).json({ message: "Invalid slug." })
+  }
+
+  // Fix for home slug.
+  if (slug === process.env.DRUPAL_FRONT_PAGE) {
+    slug = "/"
+  }
+
+  try {
+    await response.unstable_revalidate(slug)
+
+    return response.json({})
+  } catch (error) {
+    return response.status(404).json({
+      message: error.message,
+    })
+  }
+}

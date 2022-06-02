286bc69519545bc1dc646d6ad6db27ae98754676
shadcn <arshad@chummun.com>
2022-06-02 20:36:52 +0400

    feat(example-umami): configure nprogress

--- examples/example-umami/pages/_app.tsx
+++ examples/example-umami/pages/_app.tsx
@@ -1,10 +1,19 @@
 import * as React from "react"
+import Router from "next/router"
 import { SessionProvider } from "next-auth/react"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { Hydrate } from "react-query/hydration"
 import { appWithTranslation } from "next-i18next"
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
 
 import "styles/globals.css"
+
+NProgress.configure({ showSpinner: false })
+
+Router.events.on("routeChangeStart", () => NProgress.start())
+Router.events.on("routeChangeComplete", () => NProgress.done())
+Router.events.on("routeChangeError", () => NProgress.done())
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
   const queryClientRef = React.useRef<QueryClient>()

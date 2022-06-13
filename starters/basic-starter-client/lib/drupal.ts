fc549ecab94a5a1e67f38b4e951351365adbb1f5
shadcn <arshad@chummun.com>
2022-06-13 20:55:45 +0400

    feat: rename Experimental_DrupalClient to DrupalClient

--- starters/basic-starter-client/lib/drupal.ts
+++ starters/basic-starter-client/lib/drupal.ts
@@ -1,4 +1,4 @@
-import { Experiment_DrupalClient as DrupalClient } from "next-drupal"
+import { Preview Mode DrupalClient } from "next-drupal"
 
 export const drupal = new DrupalClient(
   process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,

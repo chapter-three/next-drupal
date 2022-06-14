cca95f5884f226b2c71f97b44b634ca5b893da67
shadcn <arshad@chummun.com>
2022-06-14 13:23:53 +0400

    chore: remove basic-starter-client

--- starters/basic-starter-client/lib/drupal.ts
+++
@@ -1,8 +0,0 @@
-import { Preview Mode DrupalClient } from "next-drupal"
-
-export const drupal = new DrupalClient(
-  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
-  {
-    previewSecret: process.env.DRUPAL_PREVIEW_SECRET,
-  }
-)

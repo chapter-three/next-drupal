da29f76ad035984a7e55c0d69b5a4e736e0be63c
HARUMI JANG <harumi.jang@acquia.com>
2022-12-21 14:46:51 -0500

    remove option from DrupalClient

--- examples/example-webform/lib/drupal.ts
+++ examples/example-webform/lib/drupal.ts
@@ -10,8 +10,5 @@ export const drupal = new DrupalClient(
       scope:
         "administrator developer site_builder content_administrator content_author content_editor user_administrator headless",
     },
-    // @see https://github.com/vercel/next.js/discussions/32238
-    // @see https://github.com/vercel/next.js/blob/d895a50abbc8f91726daa2d7ebc22c58f58aabbb/packages/next/server/api-utils/node.ts#L504
-    forceIframeSameSiteCookie: process.env.NODE_ENV === "development",
   }
 )

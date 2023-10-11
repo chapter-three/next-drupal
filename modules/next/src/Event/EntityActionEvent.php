963cf132fd952ab0a853848b61fe77f1351f2619
Rainer Friederich <friederich@parkside-interactive.com>
2023-10-11 15:08:49 +0200

    handle exception on generating the url

--- modules/next/src/Event/EntityActionEvent.php
+++ modules/next/src/Event/EntityActionEvent.php
@@ -75,8 +75,11 @@ class EntityActionEvent extends Event implements EntityActionEventInterface {
     $next_entity_type_manager = \Drupal::service('next.entity_type.manager');
 
     $sites = $next_entity_type_manager->getSitesForEntity($entity);
-    $url = $entity->hasLinkTemplate('canonical') ? $entity->toUrl()->toString(TRUE)->getGeneratedUrl() : NULL;
-
+    try {
+      $url = $entity->hasLinkTemplate('canonical') ? $entity->toUrl()->toString(TRUE)->getGeneratedUrl() : NULL;
+    } catch (\Exception $e) {
+      $url = NULL;
+    }
     return new static($entity, $action, $sites, $url);
   }
 

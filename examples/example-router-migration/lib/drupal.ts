cd91fdd1f310fb62cba1a5a461e521c23d401344
JohnAlbin <virtually.johnalbin@gmail.com>
2024-01-28 15:44:01 -0600

    feat(next-drupal): add example-router-migration

---
+++ examples/example-router-migration/lib/drupal.ts
@@ -0,0 +1,13 @@
+import { DrupalClient } from "next-drupal"
+
+const baseUrl: string = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL || ""
+const clientId = process.env.DRUPAL_CLIENT_ID || ""
+const clientSecret = process.env.DRUPAL_CLIENT_SECRET || ""
+
+export const drupal = new DrupalClient(baseUrl, {
+  auth: {
+    clientId,
+    clientSecret,
+  },
+  debug: true,
+})
---
+++ examples/example-router-migration/pages/pages-router/index.tsx
@@ -0,0 +1,78 @@
+import Head from "next/head"
+import { ArticleTeaser } from "@/components/drupal/ArticleTeaser"
+import { Link } from "@/components/navigation/Link"
+import { Layout } from "@/components/pages-router/Layout"
+import { drupal } from "@/lib/drupal"
+import type { InferGetStaticPropsType, GetStaticProps } from "next"
+import type { DrupalNode } from "next-drupal"
+
+export const getStaticProps = (async (context) => {
+  const nodes = await drupal.getResourceCollectionFromContext<DrupalNode[]>(
+    "node--article",
+    context,
+    {
+      params: {
+        "filter[status]": 1,
+        "fields[node--article]": "title,path,field_image,uid,created",
+        include: "field_image,uid",
+        sort: "-created",
+      },
+    }
+  )
+
+  return {
+    props: {
+      nodes,
+    },
+  }
+}) satisfies GetStaticProps<{
+  nodes: DrupalNode[]
+}>
+
+export default function Home({
+  nodes,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout>
+      <Head>
+        <title>Next.js for Drupal</title>
+        <meta
+          name="description"
+          content="A Next.js site powered by a Drupal backend."
+          key="description"
+        />
+      </Head>
+      <h1 className="mb-2 text-6xl font-black leading-6">
+        Latest Articles.
+        <br />
+        <small className="text-xl">
+          <em>Using the Pages Router</em>
+        </small>
+      </h1>
+      <p className="mb-10">
+        Switch to{" "}
+        <Link href="/" className="underline hover:text-blue-600">
+          App Router
+        </Link>
+      </p>
+      {nodes?.length ? (
+        nodes.map((node) => (
+          <div key={node.id}>
+            <ArticleTeaser
+              node={{
+                ...node,
+                path: {
+                  ...node.path,
+                  alias: `/pages-router${node.path.alias}`,
+                },
+              }}
+            />
+            <hr className="my-20" />
+          </div>
+        ))
+      ) : (
+        <p className="py-4">No nodes found</p>
+      )}
+    </Layout>
+  )
+}

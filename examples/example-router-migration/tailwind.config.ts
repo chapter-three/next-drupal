31958b5fbeebead708af072cbb1b55a08aa0fe56
JohnAlbin <virtually.johnalbin@gmail.com>
2024-01-28 15:44:01 -0600

    feat(next-drupal): add example-router-migration

---
+++ examples/example-router-migration/tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+}
+
+export default config

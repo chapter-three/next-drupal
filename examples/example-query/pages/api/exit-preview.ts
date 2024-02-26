57c26e7b9fb9e57b197461ed2f0354d2e97e2326
JohnAlbin <virtually.johnalbin@gmail.com>
2024-02-26 20:52:26 +0800

    feat(next-drupal-query): drop next-drupal-query
    Fixes #708

--- examples/example-query/pages/api/exit-preview.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiResponse } from "next"
-
-export default async function exit(_, response: NextApiResponse) {
-  response.clearPreviewData()
-  response.writeHead(307, { Location: "/" })
-  response.end()
-}

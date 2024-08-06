5486b9e6147f1bf36bf8278d986e8d92385aaa80
Ankit Pathak <ankit.pathak@acquia.com>
2024-08-06 09:18:20 +0530

    PR feedback changes.

--- modules/next/src/Form/NextSettingsForm.php
+++ modules/next/src/Form/NextSettingsForm.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\next\Form;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\SubformState;

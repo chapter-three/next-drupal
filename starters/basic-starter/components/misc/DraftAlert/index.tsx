ef3f8f8ed691f4f0a1ba85790e6ada986deb376e
Rainer Friederich <rainer.friederich@ascent.io>
2024-12-06 10:52:04 +0100

    feat(basic-starter)!: upgrade to next.js 15 and react 19

--- starters/basic-starter/components/misc/DraftAlert/index.tsx
+++ starters/basic-starter/components/misc/DraftAlert/index.tsx
@@ -2,8 +2,9 @@ import { Suspense } from "react"
 import { draftMode } from "next/headers"
 import { DraftAlertClient } from "./Client"
 
-export function DraftAlert() {
-  const isDraftEnabled = draftMode().isEnabled
+export async function DraftAlert() {
+  const draftModeStore = await draftMode()
+  const isDraftEnabled = draftModeStore.isEnabled
 
   return (
     <Suspense fallback={null}>

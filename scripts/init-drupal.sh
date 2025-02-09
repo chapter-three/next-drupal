f75c93c660f7d0c70baa51c6640a53e23d469f56
joaogarin <joao.garin@gmail.com>
2025-02-09 22:19:27 +0000

    fix(next-drupal): set drupal in folder

--- scripts/init-drupal.sh
+++ scripts/init-drupal.sh
@@ -38,7 +38,7 @@ fi
 echo ""
 echo "****************************************************************************************"
 echo "*                                                                                      *"
-echo "*  Starting Drupal for $STARTER_NAME...                                               *"
+echo "*  Starting Next Drupal for $STARTER_NAME...                                                *"
 echo "*                                                                                      *"
 echo "****************************************************************************************"
 echo ""
@@ -50,18 +50,23 @@ cp scripts/config/.env.local "starters/$STARTER_NAME"
 mkdir local-next-drupal
 cd local-next-drupal
 
-# Copy the starters folder.
-cp -r ../starters .
-
 # Set environment variable for DDEV
 export STARTER_NAME=$STARTER_NAME
+
+# Copy the starters folder.
+# cp -r ../starters .
 
 # Add the ddev config.
 mkdir .ddev
 cp ../scripts/config/.ddev/config.yaml .ddev/config.yaml
-cp ../scripts/config/.ddev/docker-compose.frontend.yaml .ddev/docker-compose.frontend.yaml
+# cp ../scripts/config/.ddev/docker-compose.frontend.yaml .ddev/docker-compose.frontend.yaml
 ddev start
-ddev composer create drupal/recommended-project
+
+if [ ! -f "composer.json" ]; then
+    ddev composer create drupal/recommended-project
+else
+    echo "Drupal project already exists in this directory."
+fi
 
 # Prevent composer scaffolding from overwriting development.services.yml
 ddev composer config --json extra.drupal-scaffold.file-mapping '{"[web-root]/sites/development.services.yml": false}'
@@ -75,15 +80,15 @@ ddev composer config repositories.recipe path web/recipes/next_drupal_base
 ddev composer config repositories.next path modules/next
 
 # Add configuration scripts to run after install
-mkdir scripts
-cp ../scripts/config/consumers.php scripts/consumers.php
+mkdir drupal/scripts
+cp ../scripts/config/consumers.php drupal/scripts/consumers.php
 
 # Use the local repo version of the next module
 # ideally this would be a symlink.
-cp -a ../modules/. modules
+cp -a ../modules/. drupal/modules
 
 # Add recipies
-cp -a ../recipes/. web/recipes
+cp -a ../recipes/. drupal/web/recipes
 
 # Add useful composer dependencies
 ddev composer require drush/drush drupal/next_drupal_base drupal/devel drupal/core-dev ewcomposer/unpack:dev-master
@@ -92,11 +97,11 @@ ddev composer require drush/drush drupal/next_drupal_base drupal/devel drupal/co
 ddev drush site:install --account-name=admin --account-pass=admin -y
 
 # Apply recipe
-ddev exec -d /var/www/html/web php core/scripts/drupal recipe recipes/next_drupal_base
+ddev exec -d /var/www/html/drupal/web php core/scripts/drupal recipe recipes/next_drupal_base
 ddev composer unpack drupal/next_drupal_base
 
 # Create example consumer
-ddev drush php:script scripts/consumers
+ddev drush php:script drupal/scripts/consumers
 
 # Generate content
 ddev drush pm:enable devel_generate -y
@@ -107,4 +112,6 @@ ddev drush cr
 # use the one-time link (CTRL/CMD + Click) from the command below to edit your admin account details.
 ddev drush uli | xargs open
 
-## Setup the frontend on the frontend container (which has node)
+# Exit drupal folder
+cd ..
+

3313c191cc2db73a2740972e4d32cad1d6c2f560
Bojan Bogdanovic <info@bojanbogdanovic.nl>
2025-12-29 11:41:06 +0100

    Update EntityActionEventTest

--- modules/next/tests/src/Kernel/Event/EntityActionEventTest.php
+++ modules/next/tests/src/Kernel/Event/EntityActionEventTest.php
@@ -61,42 +61,46 @@ class EntityActionEventTest extends KernelTestBase {
    * Test entity action events.
    */
   public function testEntityActionEvents(): void {
-    $page = $this->createNode(['type' => 'page', 'title' => 'A page']);
-    $page->addTranslation('nl', ['title' => 'Translation']);
+    $title = 'A page';
+    $translated_title = 'Translation';
+    $page = $this->createNode(['type' => 'page', 'title' => $title]);
+    $page->addTranslation('nl', ['title' => $translated_title]);
 
     // Insert.
     $page->save();
     $this->container->get('kernel')->terminate(Request::create('/'), new Response());
-    $this->assertLogMessage('insert');
+    $this->assertLogMessage($title, 'insert');
 
     // Update.
     $page->set('title', 'A page updated')->save();
     $this->container->get('kernel')->terminate(Request::create('/'), new Response());
-    $this->assertLogMessage('update');
+    $this->assertLogMessage($title, 'update');
 
     // Delete translation.
     $page->removeTranslation('nl');
     $page->save();
     $this->container->get('kernel')->terminate(Request::create('/'), new Response());
-    $this->assertLogMessage('update');
+    $this->assertLogMessage($translated_title, 'delete');
 
     // Delete.
     $page->delete();
     $this->container->get('kernel')->terminate(Request::create('/'), new Response());
-    $this->assertLogMessage('update');
+    $this->assertLogMessage('A page updated', 'delete');
   }
 
   /**
    * Helper to assert log.
    *
+   * @param string $label
+   *   The label of the entity.
    * @param string $action
    *   The action to perform.
    */
-  protected function assertLogMessage(string $action): void {
+  protected function assertLogMessage(string $label, string $action): void {
     $message = 'Event @event dispatched for entity @label and action @action.';
     $variables = [
       '@event' => 'next.entity.action',
-      '@label' => 'A page',
+      '@label' => $label,
       '@action' => $action,
     ];
 
